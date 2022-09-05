bf028648e47b2335378ef834bb8f6e55068213be
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-09-05 16:09:47 +0530

    updated the code

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -4,11 +4,11 @@ public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
         PrimeChecker primeChecker = number1->{
-            if(number <= 1){
+            if(number1 <= 1){
                 return false;
             }
-            for (int i = 2; i < number; i++) {
-                if(number % i == 0){
+            for (int i = 2; i < number1; i++) {
+                if(number1 % i == 0){
                     return false;
                 }
             }
